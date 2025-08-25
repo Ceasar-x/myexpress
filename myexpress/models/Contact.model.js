ffeb80c9fb5bb8025c9ee409e8ac652ec9ff59c0
unknown <kammaldeen5@gmail.com>
2025-08-25 12:07:45 +0100

    update

--- myexpress/models/Contact.model.js
+++ myexpress/models/Contact.model.js
@@ -4,7 +4,7 @@ const ContactSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true, unique: true },
   },
   { timestamps: true }
 );
